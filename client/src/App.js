186a8e28683fa22e8e9dc9e86e87185e06ff7708
oliviamdickerson13 <olivia.dickerson@stonybrook.edu>
2022-02-21 15:57:40 -0500

    idk

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 // import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-import AppToolbar from './AppToolbar.js'
+import AppToolbar from './AppToolbar.js';
 import MapWrapper from './MapWrapper.js';
 //import TNDistricts from './districts/TNDistricts.js';
 
 function App() {
   return (
     <div className="App">
-        <AppToolbar/>
-        <MapWrapper/>
+      <AppToolbar/>
+      <MapWrapper/>
       {/* <ReactMapGL
         mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
         mapStyle={process.env.REACT_APP_MAPBOX_STYLE}
