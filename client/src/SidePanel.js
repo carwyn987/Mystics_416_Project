9ade07dbeaafe498210a70011fb626e79ed68363
apizzulli <anttt.pizzulli@gmail.com>
2022-02-23 17:50:29 -0500

    added demo image and close button to sidePanel

--- client/src/SidePanel.js
+++ client/src/SidePanel.js
@@ -2,6 +2,7 @@ import Typography from '@mui/material/Typography';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import CloseIcon from '@mui/icons-material/Close';
 import { GlobalStore } from './dataStore'
+import graph1 from './demo-data/dem-votes-graph.png'
 import { createContext, useContext, useState } from 'react'
 
 export default function SidePanel(){
@@ -22,6 +23,7 @@ export default function SidePanel(){
             <Typography style={{fontSize:'x-large',marginTop:'2%',justifyContent:'center',paddingRight:'4%'}}>
                 Select the data to view<ArrowDropDownIcon style={{fontSize:'large'}}></ArrowDropDownIcon>
             </Typography>
+            <img src={graph1}></img>
         </div>
     );
 }
